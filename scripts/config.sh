db70a81b712d92b39898f003b70911df804440e8
Jesús del Peso Quiroga <jpquiroga@gmail.com>
2019-05-15 23:27:43 +0200

    Execution scripts

---
+++ scripts/config.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PATH=$HOME/ssd3/github_projects/tensor2tensor/tensor2tensor/bin:$PATH
+export PYTHONPATH=$HOME/ssd3/github_projects/tensor2tensor:$PYTHONPATH
---
+++ scripts/run_data_gen_translate_enes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+PROBLEM=translate_enes_opensubtitles32k
+MODEL=transformer
+HPARAMS=transformer_base_single_gpu
+
+BASE_DIR=$HOME/ssd3
+DATA_DIR=$BASE_DIR/t2t_data
+TMP_DIR=$BASE_DIR/tmp/t2t_datagen
+TRAIN_DIR=$BASE_DIR/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+
+mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
+
+# Generate data
+t2t-datagen \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
+
+
